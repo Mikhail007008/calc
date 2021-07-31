2b3f6fad577055edbaf4bd4fc1ab43e2156992b1
Mikhail007008 <kasseta136@mail.ru>
2021-07-31 21:07:06 +0300

    fix(calc): fix 2nd lesson

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ const money = 50000,
 
 	budgetDay = money / 12;
 
-	console.log(typeof money, typeof income, typeof deposit);
+console.log(typeof money, typeof income, typeof deposit);
 
-	console.log(addExpenses.length);
-	
-	console.log('Период равен ' + period + ' месяцев.' + ' Цель заработать ' + mission + ' долларов.');
-	
-	console.log(addExpenses.toLowerCase(). split(', '));
-	
-	console.log(budgetDay);
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев.' + ' Цель заработать ' + mission + ' долларов.');
+
+console.log(addExpenses.toLowerCase(). split(', '));
+
+console.log(budgetDay);
 	
